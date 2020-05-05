409827290699a84a126b70c79268ee6e4dc25fb1
NathanBeals <bealsn@knights.ucf.edu>
2020-05-05 07:42:50 -0400

    Removing duplicate files

--- CSVFilteringExtra.py
+++ CSVFilteringExtra.py
@@ -4,14 +4,23 @@ import csv
 citycol = 2
 countrycol = 4
 
+def countryFilter(passedList):
+    countryList = csv.reader(open(r"Countries.csv"), delimiter=',');
+    for country in countryList:
+        if country[0] == passedList[countrycol]:
+            return True
+
+    return False
+
 def main():
     reader = csv.reader(open(r"GameDevMap.csv"), delimiter=',');
-    countryList = csv.reader(open(r"Countries.csv"), delimiter=',');
+    #countryList = csv.reader(open(r"Countries.csv"), delimiter=',');
 
+# "United States" == p[countrycol]
     # for country in countryList:
     #     filtered = filter(lambda p: p, reader)
     filtered = filter(lambda p: 
-        "United States" == p[countrycol], 
+       countryFilter(p), 
     reader)
         #result += filtered
 
